0ab80c7ab1438e963dc0a5f4edcf30828e51e43b
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-02-23 09:24:07 -1100

    solved 02_repeatStr

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let tmp = '';
+    for(let i = 0; i < num; i++)    tmp = tmp.concat(str);
+    return tmp;
 };
 
 // Do not edit below this line
