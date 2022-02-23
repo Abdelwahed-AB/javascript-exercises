b6f04fb403bda524cab67135b5e09a7e5a44d752
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-02-23 09:35:02 -1100

    Solved 03_reverseStr

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let tmp = str.split("");
+    tmp = tmp.reverse();
+    return tmp.join("");
 };
 
 // Do not edit below this line
