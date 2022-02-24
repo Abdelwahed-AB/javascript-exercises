22760989f55edc33194cc488fc6d40dafa7b5a0c
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-02-24 09:19:07 -1100

    Solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof(b)!="number" || typeof(a)!="number") return 'ERROR';
+    if (b < a){
+        let c = a;
+        a = b;
+        b = c;
+    }
+    let sum = 0;
 
+    for(; a <= b; a++){
+        sum += a;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
