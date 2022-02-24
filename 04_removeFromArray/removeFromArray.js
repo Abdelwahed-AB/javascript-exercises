436b47702cc6f124eb09716885fdace5619a9549
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-02-24 09:10:22 -1100

    Solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
 const removeFromArray = function() {
-
+    if(arguments.length < 2)
+        return "NOT ENOUGH ARGS";
+    
+    let arr = arguments[0];
+    for(let i = 1; i < arguments.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if (arr[j] === arguments[i]){
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
