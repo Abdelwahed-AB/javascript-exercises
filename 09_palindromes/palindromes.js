2d41b9e03515c9a90e63f8076ced93a1f8ec0e74
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-03-18 09:11:43 -1100

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let a = str.replace(/[,;! \.:\/\\]/g, "").toLowerCase();
+    let n = a.length;
+    for(let i in a){
+        if(a[i] != a[n-1-i]) return false;
+    }
+    return true;
 };
 
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 // Do not edit below this line
 module.exports = palindromes;
