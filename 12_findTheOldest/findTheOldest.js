b3798abba0d7dde7633f963074da99f62c618daf
AbdelwahedAB <ababdelwahed112@gmail.com>
2022-03-18 09:50:03 -1100

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let arr = people.sort((personA, personB)=>{
 
+        if('yearOfDeath' in personA)
+            var ageA = personA.yearOfDeath - personA.yearOfBirth;
+        else
+            var ageA = (new Date()).getUTCFullYear() - personA.yearOfBirth;
+        if('yearOfDeath' in personB)
+            var ageB = personB.yearOfDeath - personB.yearOfBirth;
+        else
+            var ageB = (new Date()).getUTCFullYear() - personB.yearOfBirth;
+
+        if(ageA > ageB) return -1;
+        else return 1;
+    });
+    return arr[0];
 };
 
 // Do not edit below this line
